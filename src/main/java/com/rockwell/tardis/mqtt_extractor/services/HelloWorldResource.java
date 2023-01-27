82366aaa0a434dc3eb886d81992faa4fe14187c5
Esli Vemai <Esli.Vemai@rockwellautomation.com>
2023-01-27 04:51:43 +0100

    first commit

---
+++ src/main/java/com/rockwell/tardis/mqtt_extractor/services/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package com.rockwell.tardis.mqtt_extractor.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloWorldResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+}
---
+++ src/test/java/com/rockwell/tardis/mqtt_extractor/HelloWorldResourceIT.java
@@ -0,0 +1,9 @@
+package com.rockwell.tardis.mqtt_extractor;
+
+import com.rockwell.tardis.mqtt_extractor.services.HelloWorldResourceTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloWorldResourceIT extends HelloWorldResourceTest {
+    // Execute the same tests but in packaged mode.
+}
